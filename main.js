29dc844628a31ae9322b57bec5f7a66d94e0aafe
Hari Gudapati <harikrishnagudapati1@gmail.com>
2022-09-21 08:51:59 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const navToggle = document.querySelector(".mobile-nav-toggle")
+const primaryNav = document.querySelector(".primary-navigation")
+const primaryHeader = document.querySelector(".primary-header")
+
+navToggle.addEventListener('click', () => {
+    primaryNav.hasAttribute('data-visible') ?
+        navToggle.setAttribute('aria-expanded', false) :
+        navToggle.setAttribute("aria-expanded", true)
+    primaryNav.toggleAttribute('data-visible');
+    primaryHeader.toggleAttribute("data-overlay");
+})
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+    adaptiveHeight: true,
+    dots: true,
+    arrows: false,
+    centerMode: true,
+    responsive: {
+        480: {
+            dots: false // dots enabled 1280px and up
+        }
+    }
+});
